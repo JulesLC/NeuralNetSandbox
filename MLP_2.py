20e1daa3a23fb6ba7f2e4c229dde401c062d5ef2
JulesLC <112097763+JulesLC@users.noreply.github.com>
2022-08-30 16:02:10 +0100

    updates to initialization and feedforward

--- MLP_2.py
+++ MLP_2.py
@@ -8,27 +8,17 @@ sizes = [2,3,1]
 initA = 0
 initB = 1
 
-numberofEpochs = 1000
-gain = 3.0 #learningn rate
+numberofEpochs = 1
+lr = 3.0 #learningn rate
 
 trainingInput = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]
 trainingOutput = [[0.0], [1.0], [1.0], [0.0]]
 
 trainingdata = zip(trainingInput, trainingOutput)
-
-#note: the above zip can't be a for loop because its a parrallel iterate
-#for x, y in trainingdata:
-    #print(x,y)
-
-def visualize(nodes, edges):
-    # visualization
-    G = nx.Graph()
-    #for e in edges:
-      #  G.add_edge_value(e.split(","))
-    for n in nodes:
-        G.add_node(n)
-    nx.draw(G, with_labels=True)
-    plt.show()
+trainingdata_manual = [[0.0, 0.0], [0.0],
+                       [0.0, 1.0], [1.0],
+                       [1.0, 0.0], [1.0],
+                       [1.0, 1.0], [0.0]]
 
 
 def initiate():
@@ -36,24 +26,22 @@ def initiate():
         for inputlayer in range(sizes[0]):
             inputlayer = np.array([initA, initB])
         for layer1 in range(sizes[1]):
-            layer1 = np.random.uniform(0, 1, sizes[1]) #bias term (not the threshold +/- bias), needed unless rand is set to stay positive
+            layer1 = np.random.uniform(-1, 1, sizes[1])
         for output in range(sizes[2]):
-            output = np.random.uniform(0, 1, sizes[2])
-            #outputbiasT = [np.random.randint(-1,1)] #bias threshold version
-
-
+            output = np.random.uniform(-1, 1, sizes[2])
 
         #second verison/matrix hard coded
-        weights_input_hidden = [[np.random.uniform(0, 1, 1) for col in range(sizes[1])] for row in range(sizes[0])]
-        weights_hidden_output = [[np.random.uniform(0, 1, 1) for col in range(sizes[2])] for row in range(sizes[1])]
+        weights_input_hidden = [np.random.uniform(size = (sizes[0], sizes[1]), low= -1, high=1)]
+        weights_hidden_output = [np.random.uniform(size = (sizes[1], sizes[2]), low= -1, high=1)]
+
+        bias_hidden_nodes = [np.random.uniform(size = sizes[1], low= -1, high=1)]
+        bias_output_nodes = [np.random.uniform(size = sizes[2], low= -1, high=1)]
+
         print("wih", weights_input_hidden)
         print("who", weights_hidden_output)
-
-        bias_hidden_nodes = [[np.random.uniform(0, 1, 1) for col in range(sizes[1])] for row in range(1)]
-        bias_output_nodes = [[np.random.uniform(0, 1, 1) for col in range(sizes[2])] for row in range(1)]
-
         print("bih", bias_hidden_nodes)
         print("bho", bias_output_nodes)
+
          #           0       1        2       3                     4                 5                      6
         net = [inputlayer, layer1, output, weights_input_hidden, bias_hidden_nodes, weights_hidden_output, bias_output_nodes]
         #print(net[2])
@@ -62,14 +50,17 @@ def initiate():
 
 
 def feedforward_input_to_hidden_layer(input, weights_input_hidden, bias_hidden_nodes):
-    new_hidden_a = np.dot(weights_input_hidden, input) + bias_hidden_nodes
+    #list = []
+    new_hidden_a = np.dot(input, weights_input_hidden) + bias_hidden_nodes
     activation_hidden = sigmoid(new_hidden_a)
+    #list.append(activation_hidden)
     return activation_hidden
 
-def feedforward_hidden_to_out(input, weights_hidden_output, bias_output_nodes):
-    new_output_a = np.dot(weights_hidden_output, input) + bias_output_nodes
+def feedforward_hidden_to_out(previous_layer_activations, weights_hidden_output, bias_output_nodes):
+    new_output_a = np.dot(previous_layer_activations, weights_hidden_output) + bias_output_nodes
     activation_out = sigmoid(new_output_a)
     return activation_out
+
 
 net = initiate()
 w_i2h = net[3]
@@ -77,42 +68,83 @@ b_i2h = net[4]
 w_h2o = net[5]
 b_h2o = net[6]
 
-activation_into_hidden = feedforward_input_to_hidden_layer(w_i2h, b_i2h)
-activation_into_last = feedforward_hidden_to_out(w_h2o, b_h2o)
+print("Intialized Net: ", net)
+print("------------------------------")
+
+def test(data, weights):
+    newtest = []
+    for x in range(2):
+        for each in data:
+            #print("each", each)
+            for i in each:
+                #print("i", i)
+                for w in weights:
+                    #print("W", w)
+                    test = np.dot(i, w)+2
+                newtest.append(test)
+        return newtest
 
 
-def train(trainingOutput):
-    for example in trainingOutput:
-        LastLayerError = MeasureErrorLastLayer(activation_into_last, example, w_h2o, b_h2o, activation_into_hidden)
-    BackPropogateForHiddenError(activation_into_hidden, w_i2h, w_h2o, b_i2h, LastLayerError)
-    GradientDescent()
-    update()
+def train(net, trainingInput, trainingOutput):
+    w_i2h = net[3]
+    b_i2h = net[4]
+    w_h2o = net[5]
+    b_h2o = net[6]
+
+    for e in range(numberofEpochs):
+        for input in trainingInput:
+            #get input in to each node
+            print("each input: ", input)
+            #feed forward and get activations
+            ai = feedforward_input_to_hidden_layer(input, w_i2h, b_i2h)
+            print("activations from input to hidden: ", ai)
+            aL = feedforward_hidden_to_out(ai, w_h2o, b_h2o)
+            print("activations last layer: ", aL)
+        print("_______________________________")
+
+        for output in trainingOutput:
+            print("each output: ", output)
+            print("activations being measured", ai)
+            LastLayerError = MeasureErrorLastLayer(ai, output, w_h2o, b_h2o)
+            print("error at last layer", LastLayerError)
+            deltas = BackPropogate(ai, w_i2h, w_h2o, b_i2h, LastLayerError)
+            deltas_w = deltas[0]
+            deltas_b = deltas[1]
+            GradientDescent_weights(3.0, deltas_w, len(trainingInput), w_h2o)
+            GradientDescent_weights(3.0, deltas_w, len(trainingInput), w_i2h)
+            GradientDescent_bias(3.0, deltas_b, len(trainingInput), b_h2o)
+            GradientDescent_bias(3.0, deltas_b, len(trainingInput), b_i2h)
+        return net
 
 
-def MeasureErrorLastLayer(aL, y, z, h_weights, l_bias, aIn):
-    #difference between one component of target out and one component of activation
-    #derivative of quadratic cost function is a simple difference
-    CostL = y-aL
-    #TODO: make sure that y target lines up correct with activation outs/guesses
-    # this should not be a for loop but a parrallel iterate (matrix difference)
+def MeasureErrorLastLayer(input, y, h_weights, l_bias):
+    # make a vector of errors from a whole layer, for each neuron j in the layer l
+    # error is ej = cost/delta z, delta z a slight perturbation versions of the weighted inputs (z) coming into the neuron j
 
-    #get z (weighted input) same as feedforward without sigmoid
-    #can use feedforward for Z or does it have to remove sigmoid forward?
-    z = np.dot(h_weights, aL) + l_bias
-    deltaL = np.dot(CostL, sigmoid_prime(z))
+    # get output activation from hidden layer (aL) by feeding forward again
+    z = np.dot(input, h_weights) + l_bias
+    aL = sigmoid(z) #is activation supposed to be the one passed through the sigmoid, or is it supposed to be z without sigmoid (to use sigmoid prime)?
 
-    #get gradient of out to hidden weight and bias for last Layer
-    dCdw_L = aIn * deltaL
-    dCdb_L = deltaL
+    Cost = y-aL
 
-    lastlayerError = [dCdw_L, dCdb_L]
-    return lastlayerError
+    gradient = np.dot(Cost, sigmoid_prime(z)) ## here multiply by learning rate?
+
+    #get deltas to w, b adjust by
+    delta_L_w = aL * gradient # * learning rate here?
+    delta_L_b = gradient
+
+    lastlayerdeltas = [delta_L_w, delta_L_b]
+    return lastlayerdeltas
 
 
-def BackPropogateForHiddenError(aIn, i_weights, h_weights, h_bias, lastLayerError):
+def BackPropogate(aIn, i_weights, h_weights, h_bias, lastLayerError):
+    # get the gradient from the cost, and move it through
+
+    #outputs (check) aIn = aL.tranpose()?
+    #error of this layer = transpose of weight matrix from last layer(l+1) * error from last layer, haddamard multipy with sigmoid prime z of this layer (l)
     #pass in the deltaL from the output layer error calc
-    deltaL = lastLayerError[1]
-    print("passed in delta L: ", deltaL)
+    deltaW = lastLayerError[1]
+    print("passed in delta L: ", deltaW)
 
     #get z of the inputs into the hidden layer
     z_hidden = np.dot(i_weights, aIn) + h_bias
@@ -126,15 +158,47 @@ def BackPropogateForHiddenError(aIn, i_weights, h_weights, h_bias, lastLayerErro
     print("transpose1 ", transpose1)
 
     #get hidden layer errors:
-    delta_h = np.dot(transpose1, deltaL) * sigmoid_prime(z_hidden)
+    delta_h = np.dot(transpose1, deltaW) * sigmoid_prime(z_hidden)
     dCdb_h = aIn * delta_h
     dCdw_h = delta_h
 
     hiddenlayerError = [dCdw_h, dCdb_h]
     return hiddenlayerError
 
-def GradientDescent():
-    
+def GradientDescent_weights(learningRate, deltas, size_total_training, weights):
+    #update the weights by the deltas
+
+    # new_weights = weights + deltas[0] * learningRate
+    # new_bias = bias + deltas[1] * learningRate
+
+    new_weights = []
+    updated_weights = []
+    for x in range(size_total_training):
+        #add the gradients to the weights
+        for w_deltas,w in zip(deltas[0], weights):
+            nw = w_deltas + w
+            new_weights.append(nw)
+            for n in new_weights:
+                next_weight = w-(learningRate/size_total_training)*n
+                updated_weights.append(next_weight)
+            return updated_weights
+
+
+#Left off here:
+def GradientDescent_bias(learningRate, deltas, size_total_training, bias):
+    #update the weights by the deltas
+    new_biases = []
+    updated_biases = []
+    for x in range(size_total_training):
+        #add the gradients to the weights
+        for b_deltas,b in zip(deltas[1], bias):
+            bw = b_deltas + b
+            new_biases.append(bw)
+            for n in new_biases:
+                next_weight = b-(learningRate/size_total_training)*n
+                updated_biases.append(next_weight)
+            return updated_biases
+
 
 #### Miscellaneous functions
 def sigmoid(z):
@@ -145,3 +209,24 @@ def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
 
+def visualize(nodes, edges):
+    # visualization
+    G = nx.Graph()
+    #for e in edges:
+      #  G.add_edge_value(e.split(","))
+    for n in nodes:
+        G.add_node(n)
+    nx.draw(G, with_labels=True)
+    plt.show()
+
+
+
+
+xdata = [[1, 1], [0, 0]]
+xweights = [[5, 5, 5], [5, 5, 5]]
+print("----- test")
+print("   test", test(xdata, xweights))
+print("________________________________")
+
+trained_net = train(net, trainingInput, trainingOutput)
+print("Trained net: ", trained_net)
