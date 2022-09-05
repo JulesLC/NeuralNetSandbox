9a561d74f75b420e299673f2c86b0b72ada6dfcb
JulesLC <112097763+JulesLC@users.noreply.github.com>
2022-09-05 16:53:11 +0100

    two test images from each image

--- testimgs.py
+++ testimgs.py
@@ -1,21 +1,40 @@
 import numpy as np
 import pylab as pl
 D = np.load('testrun.npz')
-x=D['x']
-y=D['y']
-R=D['R']
+x1=D['x1']
+y1=D['y1']
+R1=D['R1'] #R is response
+
+x2=D['x2']
+y2=D['y2']
+R2=D['R2'] #R is response
+
+
 errs=D['errs']
-n = len(x)
+n1 = len(x1)
+n2 = len(x2)
+
+Rrounded1 = (R1>0.5)*1
+Rrounded2 = (R2>0.5)*1
+
+
 
 F = pl.figure()
-f = F.add_subplot(121)
+f = F.add_subplot(131)
 f.plot(errs)
 
-f = F.add_subplot(122)
-for i in range(n):
-    if(R[i]>0.51):
-        f.plot(x[i],1-y[i],'.',markersize=0.5,color='b')
-f.axis([0,+1,0,+1])
+f = F.add_subplot(132)
+j = np.random.permutation(n1)
+for i in range(5000): # n is all
+    f.plot(x1[j[i]], -y1[j[i]], '.', markersize=2,color=Rrounded1[j[i]])
+
+f.set_aspect(np.diff(f.get_xlim())/np.diff(f.get_ylim()))
+
+f = F.add_subplot(133)
+j = np.random.permutation(n2)
+for i in range(5000): # n is all
+    f.plot(x2[j[i]], -y2[j[i]], '.', markersize=2, color=Rrounded2[j[i]])
+
+f.set_aspect(np.diff(f.get_xlim())/np.diff(f.get_ylim()))
 
 pl.show()
-
